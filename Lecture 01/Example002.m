fff0caadbec03c6ac3f4a1dbf8977e75b47bc361
NhanKiet <nhankiet1996@gmail.com>
2017-09-29 10:11:05 +0700

    Dư dấu ";" - tập 2

--- Lecture 01/Example002.m
+++ Lecture 01/Example002.m
@@ -3,11 +3,11 @@ function Example002()
     m = input(strMessage);
     strMessage = '\n Nhap n:';
     n = input(strMessage);
-    a = ones(m, n);
-    b =  zeros(m, n);
-    c = eye(m, n);
-    d = randi([-10 10], 1, 10);
-    a(1,1) = 5;
-    e = size(a);
+    a = ones(m, n)
+    b =  zeros(m, n)
+    c = eye(m, n)
+    d = randi([-10 10], 1, 10)
+    a(1,1) = 5
+    e = size(a)
     
 end
